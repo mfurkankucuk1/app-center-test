9223d9b7cbc1a3cf211f1bc6bbb05f57e44328dd
furkan5443905 <furkan@eywin.com.tr>
2023-10-07 01:13:45 +0300

    add new dependencies

--- app/src/main/java/tr/mfk/appcentertest/MainActivity.kt
+++ app/src/main/java/tr/mfk/appcentertest/MainActivity.kt
@@ -1,11 +1,19 @@
 package tr.mfk.appcentertest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "c71e882e-a227-490e-b669-c2f2382529c4",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
